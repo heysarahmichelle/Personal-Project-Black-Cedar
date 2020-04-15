c8fcc8359528320ff441f29e98dc8baa3686bb83
sarahcrox <croxford.sarahm@gmail.com>
2020-04-15 16:14:12 -0600

    Trying to add Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { HashRouter } from "react-router-dom";
 import routes from "./routes";
 import HomePage from './components/Home Page/HomePage';
+import Shop from './components/Shop/Shop';
 
 
 
@@ -17,6 +18,8 @@ class App extends Component {
     return (
       <HashRouter>
       <div>
+        <Shop />
+
         <HomePage />
         {routes}
       </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,141 @@
 import React, { Component } from 'react';
 import './shop.scss';
-
+import Announcement from '../Home Page/Announcement/Announcement';
+import Header from '../Home Page/Header/Header';
+import BottomTabs from '../Home Page/Header/Bottom Tabs/BottomTabs';
+import ContactUs from '../Home Page/Contact Us/ContactUs';
 
 
 class Shop extends Component {
     render() {
         return (
-            <div>
-                Shop
+            <div className="shop-component">
+                <Announcement />
+                <Header />
+                <BottomTabs />
+                <h1>
+                    ALL
+                </h1>
+                <div className="featured-photos">
+                    <section className="feature-one">
+                        <section className="rectangle-one">
+                        </section>
+                        <p className="product-name">THE BLACK BEAR</p>
+                        <p className="product-price">$199.99</p>
+                    </section>
+
+                    <section className="feature-two">
+                        <section className="rectangle-two">
+                        </section>
+                        <p className="product-name">THE MAMA BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-three">
+                        <section className="rectangle-three">
+                        </section>
+                        <p className="product-name">THE BABY BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-four">
+                        <section className="rectangle-four">
+                        </section>
+                        <p className="product-name">THE GRIZZLY BEAR</p>
+                        <p className="product-price">$399.99</p>
+                    </section>
+                </div>
+                <div className="featured-photos">
+                    <section className="feature-one">
+                        <section className="rectangle-one">
+                        </section>
+                        <p className="product-name">THE KODIAK CAR KIT</p>
+                        <p className="product-price">$199.99</p>
+                    </section>
+
+                    <section className="feature-two">
+                        <section className="rectangle-two">
+                        </section>
+                        <p className="product-name">THE KOALA BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-three">
+                        <section className="rectangle-three">
+                        </section>
+                        <p className="product-name">THE PANDA BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-four">
+                        <section className="rectangle-four">
+                        </section>
+                        <p className="product-name">THE ANDEAN BEAR</p>
+                        <p className="product-price">$399.99</p>
+                    </section>
+                </div>
+                <div className="featured-photos">
+                    <section className="feature-one">
+                        <section className="rectangle-one">
+                        </section>
+                        <p className="product-name">THE ASIATIC BLACK BEAR</p>
+                        <p className="product-price">$199.99</p>
+                    </section>
+
+                    <section className="feature-two">
+                        <section className="rectangle-two">
+                        </section>
+                        <p className="product-name">THE SLOTH BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-three">
+                        <section className="rectangle-three">
+                        </section>
+                        <p className="product-name">THE SUN BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-four">
+                        <section className="rectangle-four">
+                        </section>
+                        <p className="product-name">THE SHORT-FACED BEAR</p>
+                        <p className="product-price">$399.99</p>
+                    </section>
+                </div>
+                <div className="featured-photos">
+                    <section className="feature-one">
+                        <section className="rectangle-one">
+                        </section>
+                        <p className="product-name">THE BLACK BEAR</p>
+                        <p className="product-price">$199.99</p>
+                    </section>
+
+                    <section className="feature-two">
+                        <section className="rectangle-two">
+                        </section>
+                        <p className="product-name">THE MAMA BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-three">
+                        <section className="rectangle-three">
+                        </section>
+                        <p className="product-name">THE POLAR BEAR</p>
+                        <p className="product-price">$299.99</p>
+                    </section>
+
+                    <section className="feature-four">
+                        <section className="rectangle-four">
+                        </section>
+                        <p className="product-name">THE GRIZZLY BEAR</p>
+                        <p className="product-price">$399.99</p>
+                    </section>
+                </div>
+                <ContactUs />
             </div>
         )
     }
 }
-
 
 export default Shop;
